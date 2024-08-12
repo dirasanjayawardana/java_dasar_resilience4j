34da8afb0abbca50641c354b759f3a525eb5c343
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-08-12 21:18:07 +0700

    backup

---
+++ src/main/java/dirapp/java_dasar_resilience4j/JavaDasarResilience4jApplication.java
@@ -0,0 +1,13 @@
+package dirapp.java_dasar_resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDasarResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDasarResilience4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dirapp/java_dasar_resilience4j/JavaDasarResilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package dirapp.java_dasar_resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDasarResilience4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
